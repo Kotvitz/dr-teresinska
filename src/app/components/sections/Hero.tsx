2431e59f0930d77af340e5cd92dfd369e4199dfe
Michał Kotfica <kotvitz17@gmail.com>
2026-02-02 18:27:14 +0100

    feat: add hero image section

--- src/app/components/sections/Hero.tsx
+++ src/app/components/sections/Hero.tsx
@@ -1,15 +1,16 @@
-// src/components/sections/Hero.tsx
 export default function Hero() {
   return (
     <section aria-hidden="true" className="w-full">
       <div
         className="
-          h-[45vh]
-          min-h-75
-          max-h-140
+          h-80 md:h-105 lg:h-130
           w-full
-          bg-(--brand-light-strong)
+          bg-(--surface-muted)
+          bg-cover bg-center bg-no-repeat
         "
+        style={{
+          backgroundImage: "url('hero-background.webp')",
+        }}
       />
     </section>
   );
